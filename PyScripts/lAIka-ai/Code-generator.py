b2773f7104a1e5d9d00eddbde47e4d09c5644428
Sabsowitz <878789@cognizant.com>
2023-09-15 11:50:23 -0400

    pyCrypt

--- PyScripts/lAIka-ai/Code-generator.py
+++ PyScripts/lAIka-ai/Code-generator.py
@@ -1,23 +1,31 @@
 
 
 import openai
-import  os
+import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = ""
+def generate_code():
+  try:
 
-response = openai.ChatCompletion.create(
-      model="gpt-4",
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
         messages=[
-                {
-                          "role": "user",  "content": ""
-                }
+            {
+                "role": "user",  "content": "generate a HTTP request to the server"
+            }
         ],
-          temperature=0,
-            max_tokens=1024,
-              top_p=1,
-                frequency_penalty=0,
-                  presence_penalty=0
-)
-                }
-        ]
-)
+        temperature=0,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    return response.choices[0].message.content.strip()
+  except Exception as e:
+      print(f"An error occurred: {str(e)}")
+      return None
+
+if __name__ == "__main__":
+  response = generate_code()
+  print(response)
---
+++ PyScripts/pyCrypt.py
@@ -0,0 +1,49 @@
+from cryptography.fernet import Fernet
+import sys
+
+banner = """
+
+██████╗░██████╗░░█████╗░░░░░░██╗███████╗██╗░░██╗████████╗
+██╔══██╗██╔══██╗██╔══██╗░░░░░██║██╔════╝██║░██╔╝╚══██╔══╝
+██████╔╝██████╔╝██║░░██║░░░░░██║█████╗░░█████═╝░░░░██║░░░
+██╔═══╝░██╔══██╗██║░░██║██╗░░██║██╔══╝░░██╔═██╗░░░░██║░░░
+██║░░░░░██║░░██║╚█████╔╝╚█████╔╝███████╗██║░╚██╗░░░██║░░░
+╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░
+
+██████╗░░█████╗░░██████╗██████╗░██╗░░░██╗████████╗███╗░░██╗██╗██╗░░██╗
+██╔══██╗██╔══██╗██╔════╝██╔══██╗██║░░░██║╚══██╔══╝████╗░██║██║██║░██╔╝
+██████╔╝███████║╚█████╗░██████╔╝██║░░░██║░░░██║░░░██╔██╗██║██║█████═╝░
+██╔══██╗██╔══██║░╚═══██╗██╔═══╝░██║░░░██║░░░██║░░░██║╚████║██║██╔═██╗░
+██║░░██║██║░░██║██████╔╝██║░░░░░╚██████╔╝░░░██║░░░██║░╚███║██║██║░╚██╗
+╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░░░░░╚═════╝░░░░╚═╝░░░╚═╝░░╚══╝╚═╝╚═╝░░╚═╝
+======================================================================
+Projekt Rasputnik
+pyCrypt
+version 0.0.1
+Usage: python pyCrypt.py [your_string]
+=======================================================================
+
+"""
+def encrypt_string(key, plaintext):
+
+    cipher_suite = Fernet(key)
+    encrypted_text = cipher_suite.encrypt(plaintext.encode())
+    return encrypted_text.decode()
+
+# Example usage:
+if __name__ == "__main__":
+    # Generate a random encryption key (should be kept secret)
+    encryption_key = Fernet.generate_key()
+    input = sys.argv[1]
+  
+    # Text to encrypt
+    text_to_encrypt = input
+
+    # Encrypt the text
+    encrypted_text = encrypt_string(encryption_key, text_to_encrypt)
+    # print(banner)
+    print('\033[91m' + '\033[92m', banner)
+    # print("Encrypted Text:", encrypted_text)
+    print('\033[91m'+'encrypted_text: ' + '\033[92m', encrypted_text)
+    
+
