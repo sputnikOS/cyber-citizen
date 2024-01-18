31a469425d459e55f10d4c7b53fb4c8891e83bfc
Stephen <prometheus1987@protonmail.com>
2024-01-18 02:34:33 -0500

    [PyScripts] Blockchain: combined blockchain scripts

--- PyScripts/blockchain/blockchain.py
+++ PyScripts/blockchain/blockchain.py
@@ -9,9 +9,27 @@ class Wallet:
         self.public_key = hashlib.sha256(self.private_key.encode()).hexdigest()
 
 class Token:
-    def __init__(self, name, symbol):
+    def __init__(self, name, symbol, total_supply):
         self.name = name
         self.symbol = symbol
+        self.total_supply = total_supply
+        self.balance = {}
+
+    def mint(self, account, amount):
+        if account in self.balance:
+            self.balance[account] += amount
+        else:
+            self.balance[account] = amount
+
+    def transfer(self, sender, receiver, amount):
+        if sender in self.balance and self.balance[sender] >= amount:
+            self.balance[sender] -= amount
+            if receiver in self.balance:
+                self.balance[receiver] += amount
+            else:
+                self.balance[receiver] = amount
+            return True
+        return False
 
 class Transaction:
     def __init__(self, sender, recipient, amount, token):
@@ -46,15 +64,22 @@ blockchain = [create_genesis_block()]
 previous_block = blockchain[0]
 wallet_A = Wallet()
 wallet_B = Wallet()
-my_token = Token("OSS", "OSS")
+token = Token("SAM", "SAM", 1000)
+
+# token = Token("Samson", "SAM", 1000000)
+# token.mint("Address1", 500)
+# token.transfer("Address1", "Address2", 200)
+
 
 # Add token transactions and blocks
-token_transactions = [Transaction(wallet_A.public_key, wallet_B.public_key, 5, my_token),
-                      Transaction(wallet_A.public_key, wallet_B.public_key, 2, my_token)]
-new_block = create_new_block(previous_block, token_transactions)
-blockchain.append(new_block)
-previous_block = new_block
+# token_transactions = [Transaction(wallet_A.public_key, wallet_B.public_key, 5, token),
+#                       Transaction(wallet_A.public_key, wallet_B.public_key, 2, token)]
+# new_block = create_new_block(previous_block, token_transactions)
+# blockchain.append(new_block)
+# previous_block = new_block
 
 # Print wallet balances and token amounts
-print(f"Wallet A Balance: {wallet_A.balance}, Token Amount: {wallet_A.tokens}")
+print(f"Wallet A Balance: {wallet_A.balance}, Token Amount: {wallet_A.tokens}, Address: {wallet_A.public_key}")
 print(f"Wallet B Balance: {wallet_B.balance}, Token Amount: {wallet_B.tokens}")
+
+print(f"Token Supply: {token.total_supply}")
