5b31769dfb33373e3ade2e940508fe080e5bea2c
Stephen <prometheus1987@protonmail.com>
2024-01-18 03:12:58 -0500

    [PyScripts - Blockchain]

--- PyScripts/blockchain/blockchain.py
+++ PyScripts/blockchain/blockchain.py
@@ -1,5 +1,6 @@
 import hashlib
 import time
+import argparse
 
 class Wallet:
     def __init__(self):
@@ -61,17 +62,16 @@ def create_new_block(previous_block, transactions):
 
 # Example Usage:
 blockchain = [create_genesis_block()]
+token = Token("Laika", "SAM", 1000)
 previous_block = blockchain[0]
 wallet_A = Wallet()
 wallet_B = Wallet()
-token = Token("SAM", "SAM", 1000)
 
-# token = Token("Samson", "SAM", 1000000)
-# token.mint("Address1", 500)
-# token.transfer("Address1", "Address2", 200)
+# token.mint(wallet_A.public_key, 500)
+# token.transfer(wallet_A.public_key, wallet_B.public_key, 200, token)
 
 
-# Add token transactions and blocks
+# # Add token transactions and blocks
 # token_transactions = [Transaction(wallet_A.public_key, wallet_B.public_key, 5, token),
 #                       Transaction(wallet_A.public_key, wallet_B.public_key, 2, token)]
 # new_block = create_new_block(previous_block, token_transactions)
@@ -79,7 +79,7 @@ token = Token("SAM", "SAM", 1000)
 # previous_block = new_block
 
 # Print wallet balances and token amounts
-print(f"Wallet A Balance: {wallet_A.balance}, Token Amount: {wallet_A.tokens}, Address: {wallet_A.public_key}")
-print(f"Wallet B Balance: {wallet_B.balance}, Token Amount: {wallet_B.tokens}")
+print(f"\033[91m\tWallet A Balance: {wallet_A.balance}, Token Amount: {wallet_A.tokens}, Address: {wallet_A.public_key}")
+print(f"\033[91m\tWallet B Balance: {wallet_B.balance}, Token Amount: {wallet_B.tokens}, Address: {wallet_B.public_key}")
 
-print(f"Token Supply: {token.total_supply}")
+print(f"\033[91m\tToken Supply: {token.total_supply}")
