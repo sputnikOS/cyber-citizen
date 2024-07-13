2241f624ef43a5ba92766edaaa0c7fe37832fa3a
Stephen Sabsowitz <prometheus1987@protonmail.com>
2024-07-12 17:57:53 -0700

    CLang

--- scripts/Ruby/encryption/ssh-keygen.rb
+++ scripts/Ruby/encryption/ssh-keygen.rb
@@ -1,13 +1,14 @@
 require 'openssl'
-
+require 'colorize'
 # Generate a 2048-bit RSA key
 key = OpenSSL::PKey::RSA.generate(2048)
 
 # Get the public key
 public_key = key.public_key
 
+str = "\n#==============================================================="
 # Print the public key
-puts "Public Key:\n#{public_key}"
-
-# Print the private key (for demonstration purposes)
+puts "Public Key" + ":\n#{public_key}"
+puts str.colorize(:green)
+ # Print the private key (for demonstration purposes)
 puts "Private Key:\n#{key}"
