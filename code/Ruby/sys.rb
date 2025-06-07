9f10f8b48afda70973244094c79a5d5c25177b9c
Stephen Sabsowitz <prometheus1987@protonmail.com>
2025-06-06 22:39:12 -0400

    sys.rb

--- code/Ruby/sys.rb
+++ code/Ruby/sys.rb
@@ -4,15 +4,28 @@ require 'terminal-table'
 require 'io/console'
 
 def clear_terminal
-  if Gem.win_platform?
-    system("cls")  # Clear command for Windows
-  else
-    system("clear")  # Clear command for Unix-based systems
-  end
+  system(Gem.win_platform? ? 'cls' : 'clear')
+end
+
+def terminal_width
+  IO.console.winsize[1] rescue 100
+end
+
+def center_line(line)
+  padding = [(terminal_width - line.length) / 2, 0].max
+  ' ' * padding + line
+end
+
+def center_text(text, color = :default)
+  puts center_line(text).colorize(color)
+end
+
+def center_block(text, color = :default)
+  text.lines.each { |line| puts center_line(line.rstrip).colorize(color) }
 end
 
 def get_os_info
-  { "OS" => `uname -s`.strip, "Version" => `uname -r`.strip }
+  { "Operating System" => `uname -s`.strip, "Kernel Version" => `uname -r`.strip }
 end
 
 def get_cpu_info
@@ -20,62 +33,83 @@ def get_cpu_info
   cpu_details = {}
   cpu_info.each_line do |line|
     key, value = line.split(':')
-    if key && value
-      cpu_details[key.strip] = value.strip
-    end
+    cpu_details[key.strip] = value.strip if key && value
   end
-  cpu_details
+  {
+    "Model Name" => cpu_details["Model name"],
+    "Cores" => cpu_details["CPU(s)"],
+    "Architecture" => cpu_details["Architecture"],
+    "CPU MHz" => cpu_details["CPU MHz"]
+  }
 end
 
 def get_memory_info
-  { "Total Memory" => `free -h | grep Mem | awk '{print $2}'`.strip, "Used Memory" => `free -h | grep Mem | awk '{print $3}'`.strip }
+  {
+    "Total Memory" => `free -h | grep Mem | awk '{print $2}'`.strip,
+    "Used Memory" => `free -h | grep Mem | awk '{print $3}'`.strip,
+    "Free Memory" => `free -h | grep Mem | awk '{print $4}'`.strip
+  }
 end
 
 def get_disk_info
-  `df -h`
+  `df -h --output=source,fstype,size,used,avail,pcent,target | grep -v tmpfs`.strip
 end
 
 def get_network_info
-  { "IP Address" => `hostname -I`.strip, "MAC Address" => `cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address`.strip }
+  iface = `ip route show default | awk '/default/ {print $5}'`.strip
+  {
+    "IP Address" => `hostname -I`.strip,
+    "MAC Address" => iface.empty? ? "Unavailable" : `cat /sys/class/net/#{iface}/address`.strip
+  }
 end
 
 def get_uptime
   { "Uptime" => `uptime -p`.strip }
 end
 
-def center_text(text)
-  terminal_width = IO.console.winsize[1] rescue 100  # Default to 100 if terminal width can't be determined
-  padding = [(terminal_width - text.length) / 2, 0].max
-  puts " " * padding + text
-end
-
-def display_table(title, data)
+def display_table_centered(title, data)
   rows = data.map { |key, value| [key, value] }
   table = Terminal::Table.new(title: title, rows: rows)
-  puts table
+  table.to_s.each_line { |line| puts center_line(line.rstrip) }
+end
+
+def display_disk_info_centered(disk_text)
+  disk_text.each_line { |line| puts center_line(line.rstrip).colorize(:light_green) }
 end
 
 def display_info
   clear_terminal
   artii = Artii::Base.new(font: "slant")
-  banner = artii.asciify("SputnikOS")
-  puts banner.center(140).colorize(:yellow)
+  center_block(artii.asciify("SputnikOS"), :green)
 
-  headers = ["System Information", "CPU Information", "Memory Information", "Disk Information", "Network Information", "Uptime Information"]
-  methods = [method(:get_os_info), method(:get_cpu_info), method(:get_memory_info), method(:get_disk_info), method(:get_network_info), method(:get_uptime)]
+  center_text("Generated at: #{Time.now.strftime("%A, %d %B %Y %H:%M:%S")}", :light_black)
+  puts "\n" + center_line("-" * 100)
 
-  headers.each_with_index do |header, index|
-  puts "\n" + "=" *   140
-    puts header.center(140).colorize(:yellow)
-    puts "=" * 140
-    data = methods[index].call
-    if header == "Disk Information"
-      puts data.colorize(:green)
+  sections = [
+    { title: "System Information", data: get_os_info },
+    { title: "CPU Information", data: get_cpu_info },
+    { title: "Memory Information", data: get_memory_info },
+    { title: "Disk Information", data: get_disk_info },
+    { title: "Network Information", data: get_network_info },
+    { title: "Uptime Information", data: get_uptime }
+  ]
+
+  sections.each do |section|
+    puts "\n" + center_line("=" * 100)
+    center_text(section[:title], :yellow)
+    puts center_line("=" * 100) + "\n"
+
+    if section[:title] == "Disk Information"
+      display_disk_info_centered(section[:data])
     else
-      display_table(header, data)
+      display_table_centered(section[:title], section[:data])
     end
   end
+
+  puts "\n" + center_line("-" * 100)
+  center_text("End of Report", :light_blue)
+  puts center_line("-" * 100) + "\n\n"
 end
 
-# Call the function to display system information
+# Execute
 display_info
